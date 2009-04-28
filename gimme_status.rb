e3b84fb337bc3f1cf59eb9bafb755b0c705ebd10
Chris O'Sullivan <osullc02@Chris-O-Mac.local>
2009-04-28 10:38:56 +0100

    Added ability for post

--- gimme_status.rb
+++ gimme_status.rb
@@ -2,5 +2,13 @@ require 'rubygems'
 require 'sinatra'
 
 get "/:new_status" do
-  [params[:new_status].to_i, "Gimme status #{params[:new_status]}"]
+   return_status 
+end
+
+post "/:new_status" do 
+  return_status
+end
+
+def return_status
+  [params[:new_status].to_i, "Gimme status #{params[:new_status]}"]  
 end
--- test/gimme_status_test.rb
+++ test/gimme_status_test.rb
@@ -12,9 +12,15 @@ class GimmeStatusTest < Test::Unit::TestCase
   end
     
   [404, 500, 200].each do |status|
-    define_method("test_passing_#{status}") do
+    define_method("test_get_#{status}") do
       get "/#{status}"
       assert_equal(status, last_response.status)
     end
+    
+    define_method("test_post_#{status}") do
+      post "/#{status}"
+      assert_equal(status, last_response.status)
+    end
+    
   end
 end
